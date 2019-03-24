88487d7e67f0e7256de7ccb500c1dd11974639e4
MICHAELSWONG <mizik_jah@yahoo.com>
2019-03-24 15:50:58 -0400

    Working on inserts

--- etc/StormLight.sql
+++ etc/StormLight.sql
@@ -1,4 +1,4 @@
-
+﻿
 -- Switch to the system (master) database --
 USE master;
 GO
@@ -23,38 +23,68 @@ CREATE TABLE Character
 	character_id int identity(1,1),
 	character_name varchar(100) not null,
 	abilities varchar(100),
+	bonded_with varchar(30),
 	titles varchar(1000),
 	groups varchar(1000),
-	knights_orders_name varchar(100),
+	knights_orders_name varchar(50),
 	surges varchar(1000),
+	character_image varchar(100),
 
 	constraint pk_Character primary key(character_id),	
 );
 
 CREATE TABLE Book
 (
-	book_id int identity(1,1),
-	character_id int not null,
+	book_id int identity(1,1),	
 	book_name varchar(100) not null,
 	published varchar(50),
-	back_cover varchar(5000),	
+	back_cover varchar(5000),
+	book_image varchar (100),
 
-	constraint pk_Book primary key(book_id),
-	constraint fk_Book_character foreign key(character_id) references Character(character_id)
+	constraint pk_Book primary key(book_id),	
+);
+
+CREATE TABLE Character_Book
+(
+	character_id int not null,
+	book_id int not null,
+
+	constraint fk_character_id foreign key (character_id) references Character (character_id),
+	constraint fk_book_id foreign key (book_id) references Book (book_id)
+
 );
 
 CREATE TABLE Knight_Orders
 (
-	knight_orders_id int identity(1,1),
-	character_id int not null,
-	knight_orders_name varchar(100),
+	knight_orders_id int identity(1,1),	
+	knight_orders_name varchar(50),
 	spren varchar(50) not null,
-	attributes varchar(1000),
-	surges varchar(1000),	
+	surges varchar(1000),
+	knight_orders_images varchar(100)
 
-	constraint pk_Knight_Orders primary key(knight_orders_id),
-	constraint fk_Knight_Orders_character foreign key(character_id) references Character(character_id)
+	constraint pk_Knight_Orders primary key(knight_orders_id),	
 );
+
+CREATE TABLE Knight_Orders_Characters
+(
+	knight_orders_id int not null,
+	character_id int not null,
+
+	constraint fk_knight_orders_id foreign key (knight_orders_id) references Knight_Orders (knight_orders_id),
+	constraint fk_character_id foreign key (character_id) references Character (character_id)
+);
+
+INSERT Knight_Orders VALUES ('Order of Windrunners', 'honorspren', 'Adhesion and Gravitation', 'Windrunners.jpg')
+INSERT Knight_Orders VALUES ('Order of Skybreakers', ' highspren', 'Gravitation and Division', 'Skybreakers.jpg')
+INSERT Knight_Orders VALUES ('Order of Dustbringers', 'honorspren', 'Adhesion and Gravitation', 'Dustbringers.jpg')
+INSERT Knight_Orders VALUES ('Order of Edgedancers', 'honorspren', 'Adhesion and Gravitation', 'Edgedancers.jpg')
+INSERT Knight_Orders VALUES ('Order of Truthwatchers', 'honorspren', 'Adhesion and Gravitation', 'Truthwatchers.jpg')
+INSERT Knight_Orders VALUES ('Order of Lightweavers', 'honorspren', 'Adhesion and Gravitation', 'Lightweavers.jpg')
+INSERT Knight_Orders VALUES ('Order of Elsecallers', 'honorspren', 'Adhesion and Gravitation', 'Elsecallers.jpg')
+INSERT Knight_Orders VALUES ('Order of Willshapers', 'honorspren', 'Adhesion and Gravitation', 'Willshapers.jpg')
+INSERT Knight_Orders VALUES ('Order of Stonewards', 'honorspren', 'Adhesion and Gravitation', 'Stonewards.jpg')
+INSERT Knight_Orders VALUES ('Order of Bondsmiths', 'honorspren', 'Adhesion and Gravitation', 'Bondsmiths.jpg')
+
 
 CREATE TABLE survey_result
 (
@@ -67,13 +97,34 @@ CREATE TABLE survey_result
 	constraint fk_survey_result_character foreign key (character_id) references Character (character_id)
 );
 
-INSERT Character VALUES ('CVNP', 'Cuyahoga Valley National Park', 'Ohio', 32832, 696, 125, 0, 'Woodland', 2000, 2189849, 'Of all the paths you take in life, make sure a few of them are dirt.', 'John Muir', 'Though a short distance from the urban areas of Cleveland and Akron, Cuyahoga Valley National Park seems worlds away. The park is a refuge for native plants and wildlife, and provides routes of discovery for visitors. The winding Cuyahoga River gives way to deep forests, rolling hills, and open farmlands. Walk or ride the Towpath Trail to follow the historic route of the Ohio & Erie Canal', 0, 390)
-INSERT Character VALUES ('ENP', 'Everglades National Park', 'Florida', 1508538, 0, 35, 0, 'Tropical', 1934, 1110901, 'There are no other Everglades in the world. They are, they have always been, one of the unique regions of the earth; remote, never wholly known. Nothing anywhere else is like them.', 'Marjory Stoneman Douglas', 'The Florida Everglades, located in southern Florida, is one of the largest wetlands in the world. Several hundred years ago, this wetlands was a major part of a 5,184,000 acre watershed that covered almost a third of the entire state of Florida. The Everglades consist of a shallow sheet of fresh water that rolls slowly over the lowlands and through billions of blades of sawgrass. As water moves through the Everglades, it causes the sawgrass to ripple like green waves; this is why the Everglades received the nickname "River of Grass."', 8, 760)
-INSERT Character VALUES ('GCNP', 'Grand Canyon National Park', 'Arizona', 1217262, 8000, 115, 120, 'Desert', 1919, 4756771, 'It is the one great wonders. . . every American should see.', 'Theodore Roosevelt', 'If there is any place on Earth that puts into perspective the grandiosity of Mother Nature, it is the Grand Canyon. The natural wonder, located in northern Arizona, is a window into the regio''s geological and Native American past. As one of the country''s first national parks, the Grand Canyon has long been considered a U.S. treasure, and continues to inspire scientific study and puzzlement.', 8, 450)
-INSERT Character VALUES ('GNP', 'Glacier National Park', 'Montana', 1013322, 6646, 745.6, 923, 'Temperate', 1910, 2338528, 'Far away in northwestern Montana, hidden from view by clustering mountain peaks, lies an unmapped corner�the Crown of the Continent.', 'George Bird Grinnell', 'Glacier might very well be the most beautiful of America''s national parks. John Muir called it "the best care-killing scenery on the continent." The mountains are steep, snowcapped, and punctuated by stunning mountain lakes and creeks. Much of the land remains wild and pristine, a result of its remote location and the lack of visitation in the 19th century.  ', 12, 300)
-INSERT Character VALUES ('GSMNP', 'Great Smoky Mountains National Park', 'Tennessee', 522419, 6500, 850, 939, 'Temperate', 1934, 10099276, 'May your trails be crooked, winding, lonesome, dangerous, leading to the most amazing view. May your mountains rise into and above the clouds.', 'Edward Abbey', 'The Great Smoky Mountains are a mountain range rising along the Tennessee�North Carolina border in the southeastern United States. They are a subrange of the Appalachian Mountains, and form part of the Blue Ridge Physiographic Province. The range is sometimes called the Smoky Mountains and the name is commonly shortened to the Smokies. The Great Smokies are best known as the home of the Great Smoky Mountains National Park, which protects most of the range. The park was established in 1934, and, with over 9 million visits per year, it is the most-visited national park in the United States', 0, 400)
-INSERT Character VALUES ('GTNP', 'Grand Teton National Park', 'Wyoming', 310000, 6800, 200, 1206, 'Temperate', 1929, 2791392, 'We can not have freedom without wilderness.', 'Edward Abbey', 'The peaks of the Teton Range, regal and imposing as they stand nearly 7,000 feet above the valley floor, make one of the boldest geologic statements in the Rockies. Unencumbered by foothills, they rise through steep coniferous forest into alpine meadows strewn with wildflowers, past blue and white glaciers to naked granite pinnacles. The Grand, Middle, and South Tetons form the heart of the range. But their neighbors, especially Mount Owen, Teewinot Mountain, and Mount Moran, are no less spectacular.', 15, 380)
-INSERT Character VALUES ('MRNP', 'Mount Rainier National Park', 'Washingto', 236381, 5500, 260, 573, 'Rainforest', 1899, 1038229, 'Of all the fire mountains which like beacons, once blazed along the Pacific Coast, Mount Rainier is the noblest.', 'Unknow', 'Mt. Rainier National Park is one of three national parks in the state of Washington and is one of America''s oldest parks, being one of only five founded in the 19th century. The park was created to preserve one of America''s most spectacular scenic wonders, the snow-capped volcano known as Tahcoma to Indians in ages past and as Mt. Rainier now. While the mountain is unquestionably the centerpiece of the park, its 235,612 acres (378 square miles) also contain mountain ranges, elaborate glaciers, rivers, deep forests, lush meadows covered with wildflowers during the summer, and over 300 miles of trails. 96% of the park is classified as wilderness.', 20, 280)
-INSERT Character VALUES ('RMNP', 'Rocky Mountain National Park', 'Colorado', 265761, 7800, 300, 660, 'Woodland', 1915, 3176941, 'It''s not the mountain we conquer, but ourselves.', 'Sir Edmund Hillary', 'Rocky Mountain National Park is one of the highest national parks in the nation, with elevations from 7,860 feet to 14,259 feet. Sixty mountain peaks over 12,000 feet high result in world-renowned scenery. The Continental Divide runs north - south through the park, and marks a climatic division. Ancient glaciers carved the topography into an amazing range of ecological zones. What you see within short distances at Rocky is similar to the wider landscape changes seen on a drive from Denver to northern Alaska.', 20, 360)
-INSERT Character VALUES ('YNP', 'Yellowstone National Park', 'Wyoming', 2219791, 8000, 900, 1900, 'Temperate', 1872, 3394326, 'Yellowstone Park is no more representative of America than is Disneyland.', 'John Steinbeck', 'Yellowstone National Park is a protected area showcasing significant geological phenomena and processes. It is also a unique manifestation of geothermal forces, natural beauty, and wild ecosystems where rare and endangered species thrive. As the site of one of the few remaining intact large ecosystems in the northern temperate zone of earth, Yellowstone�s ecological communities provide unparalleled opportunities for conservation, study, and enjoyment of large-scale wildland ecosystem processes.', 15, 390)
-INSERT Character VALUES ('YNP2', 'Yosemite National Park', 'California', 747956, 5000, 800, 1720, 'Forest', 1890, 3882642, 'Yosemite Valley, to me, is always a sunrise, a glitter of green and golden wonder in a vast edifice of stone and space.', 'Ansel Adams', 'Yosemite National Park vividly illustrates the effects of glacial erosion of granitic bedrock, creating geologic features that are unique in the world. Repeated glaciations over millions of years have resulted in a concentration of distinctive landscape features, including soaring cliffs, domes, and free-falling waterfalls. There is exceptional glaciated topography, including the spectacular Yosemite Valley, a 914-meter (1/2 mile) deep, glacier-carved cleft with massive sheer granite walls. These geologic features provide a scenic backdrop for mountain meadows and giant sequoia groves, resulting in a diverse landscape of exceptional natural and scenic beauty.', 15, 420)
+INSERT Character VALUES ('Kaladin Stormblessed', 'Windrunner, Shardbearer', 'Sylphrena', 'Captain of the Bridgemen, Knight Radiant', 'Knights Radiant (Windrunners), Bridge Four, Kholin army, Amaram''s army (formerly), Sadeas army (formerly), Kaladin''s squad (formerly), Takers (formerly)', 'Order of Windrunners','Kaladin.jpg')
+INSERT Character VALUES ('Shallan Davar', 'Lightweaver, Shardbearer', 'Pattern', 'Veil, Radiant', 'Knights Radiant (Lightweavers), Ghostbloods', 'Order of Lightweavers', 'Shallan.jpg')
+INSERT Character VALUES ('Dalinar Kholin', 'Bondsmith, Old Magic, Shardbearer', 'Stormfather', 'Highprince of Alethkar (formerly), Highprince of War', 'Knights Radiant (Bondsmiths), Kholin army', 'Order of Bondsmiths', 'Dalinar.jpg')
+INSERT Character VALUES ('Jasnah Kholin', 'Elsecaller, Shardbearer', 'Ivory', 'Queen of Alethkar', 'Knights Radiant (Elsecallers) Veristitalians', 'Order of Elsecallers', 'Jasnah.jpg')
+INSERT Character VALUES ('Lift', 'Edgedancer, Shardbearer, Old Magic', 'Wyndle', 'Thief', '	Huqin''s crew', 'Order of Edgedancers', 'Lift.jpg')
+
+
+INSERT Book VALUES ('A Way of Kings', 'August 31, 2010', 'I long for the days before the Last Desolation. The age before the Heralds abandoned us and the Knights Radiant turned against us. A time when there was still magic in the world and honor in the hearts of men. The world became ours, and we lost it. Nothing, it appears, is more challenging to the souls of men than victory itself. Or was that victory an illusion all along? Did our enemies realize that the harder they fought, the stronger we resisted? Perhaps they saw that the heat and the hammer only make for a better grade of sword. But ignore the steel long enough, and it will eventually rust away. There are four whom we watch. The first is the surgeon, forced to put aside healing to become a soldier in the most brutal war of our time. The second is the assassin, a murderer who weeps as he kills. The third is the liar, a young woman who wears a scholar’s mantle over the heart of a thief. The last is the highprince, a warlord whose eyes have opened to the past as his thirst for battle wanes. The world can change. Surgebinding and Shardwielding can return; the magics of ancient days can become ours again. These four people are key. One of them may redeem us. And one of them will destroy us.', 'AWayOfKings.jpg')
+INSERT Book VALUES ('Words of Radiance', 'March 14, 2014', 'The Knights Radiant must stand again. The ancient oaths have at last been spoken; the spren return. Men seek that which was lost. I fear the struggle will destroy them. It is the nature of the magic. A broken soul has cracks into which something else can be fit.  Surgebindings, the powers of creation themselves.  They can brace a broken soul; but they can also widen its fissures. The Windrunner, lost in a shattered land, balanced upon the boundary between  vengeance and honor. The Lightweaver, slowly being consumed by her past, searching for the lie that she must become. The Bondsmith, born in blood and death, striving to rebuild what was destroyed. The Explorer, straddling the fates of two peoples, forced to  choose between slow death and a terrible betrayal of all she believes. It is past time for them to awaken, for the Everstorm looms. And the Assassin has arrived.', 'WordsOfRadiance.jpg')
+INSERT Book VALUES ('Edgedancer', 'October 2017', 'Three years ago, Lift asked a goddess to stop her from growing older–a wish she believed was granted. Now, in Edgedancer, the barely teenage nascent Knight Radiant finds that time stands still for no one. Although the young Azish emperor granted her safe haven from an executioner she knows only as Darkness, court life is suffocating the free-spirited Lift, who can’t help heading to Yeddaw when she hears the relentless Darkness is there hunting people like her with budding powers. The downtrodden in Yeddaw have no champion, and Lift knows she must seize this awesome responsibility.', 'Edgedancer.jpg')
+INSERT Book VALUES ('OathBringer', 'November 14, 2017', 'Dalinar Kholin’s Alethi armies won a fleeting victory at a terrible cost: The enemy Parshendi summoned the violent Everstorm, which now sweeps the world with destruction, and in its passing awakens the once peaceful and subservient parshmen to the horror of their millennia-long enslavement by humans. While on a desperate flight to warn his family of the threat, Kaladin Stormblessed must come to grips with the fact that the newly kindled anger of the parshmen may be wholly justified. Nestled in the mountains high above the storms, in the tower city of Urithiru, Shallan Davar investigates the wonders of the ancient stronghold of the Knights Radiant and unearths dark secrets lurking in its depths. And Dalinar realizes that his holy mission to unite his homeland of Alethkar was too narrow in scope. Unless all the nations of Roshar can put aside Dalinar’s blood-soaked past and stand together―and unless Dalinar himself can confront that past―even the restoration of the Knights Radiant will not prevent the end of civilization.', 'Oathbringer.jpg')
+
+INSERT Character_Book VALUES (1, 1)
+INSERT Character_Book VALUES (1, 2)
+INSERT Character_Book VALUES (1, 3)
+INSERT Character_Book VALUES (1, 4)
+
+INSERT Character_Book VALUES (2, 1)
+INSERT Character_Book VALUES (2, 2)
+INSERT Character_Book VALUES (2, 3)
+INSERT Character_Book VALUES (2, 4)
+INSERT Character_Book VALUES (2, 5)
+
+INSERT Character_Book VALUES (3, 5)
+
+
+INSERT Character_Book VALUES (4, 1)
+INSERT Character_Book VALUES (4, 2)
+INSERT Character_Book VALUES (4, 3)
+INSERT Character_Book VALUES (4, 4)
+INSERT Character_Book VALUES (4, 5)
