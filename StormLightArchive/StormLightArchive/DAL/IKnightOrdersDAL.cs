ff798ce7c233bc9ea41885e23fd557ae273280cf
MICHAELSWONG <mizik_jah@yahoo.com>
2019-03-24 19:59:51 -0400

    adding dals

---
+++ StormLightArchive/StormLightArchive/DAL/IKnightOrdersDAL.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using StormLightArchive.Models;
+
+namespace StormLightArchive.DAL
+{
+    public interface IKnightOrdersDAL
+    {
+        List<KnightOrdersModel> GetAllKnightOrders();
+
+        KnightOrdersModel GetKnightOrder(string name);
+    }
+}
--- StormLightArchive/StormLightArchive/Models/KnightOrdersModel.cs
+++ StormLightArchive/StormLightArchive/Models/KnightOrdersModel.cs
@@ -13,6 +13,8 @@ namespace StormLightArchive.Models
         public string Spren { get; set; }
         public string Surges { get; set; }
         public string Knight_Orders_Images { get; set; }
+        public int Character_Id { get; set; }
+        public string Character_Name { get; set; }
 
     }
 }
